1d2a6f495314628b92852e2467cae7c9578dfdec
FrikoGad <goliaf551@mail.ru>
2021-06-04 23:43:18 +0300

    commit образец

--- src/main/java/book/lesson/java/Firslesson.java
+++ src/main/java/book/lesson/java/Firslesson.java
@@ -3,8 +3,6 @@ package book.lesson.java;
 public class Firslesson {
     public static void main(String[] args) {
         int a = 5;
-
-
         System.out.println(a + " Простая программа на Java");
     }
 
