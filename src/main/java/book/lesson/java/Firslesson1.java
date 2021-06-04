1a4e7d62865a78b5a0c5c9b06b19239b4aeefdd5
FrikoGad <goliaf551@mail.ru>
2021-06-05 00:02:49 +0300

    Обучение

--- src/main/java/book/lesson/java/Firslesson1.java
+++ src/main/java/book/lesson/java/Firslesson1.java
@@ -1,6 +1,6 @@
 package book.lesson.java;
 
-public class Firslesson {
+public class Firslesson1 {
     public static void main(String[] args) {
         int a = 5;
         System.out.println(a + " Простая программа на Java");
