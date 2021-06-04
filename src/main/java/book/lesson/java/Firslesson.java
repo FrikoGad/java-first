f215404078983761c044a75fc004a8ed7b6afdf0
FrikoGad <goliaf551@mail.ru>
2021-06-04 23:29:12 +0300

    Книга для изучения Java (72 стр.)

--- src/main/java/book/lesson/java/Firslesson.java
+++ src/main/java/book/lesson/java/Firslesson.java
@@ -3,6 +3,8 @@ package book.lesson.java;
 public class Firslesson {
     public static void main(String[] args) {
         int a = 5;
-        System.out.println(a + "Простая программа на Java");
+        System.out.println(a + " Простая программа на Java");
     }
+
+
 }
